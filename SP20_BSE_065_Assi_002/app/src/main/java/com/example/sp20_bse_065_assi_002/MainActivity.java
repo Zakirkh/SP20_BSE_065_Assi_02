01fa38c12404333f3c1ed36194db35e1376f2a5a
Zakirkh <104098509+Zakirkh@users.noreply.github.com>
2022-04-24 20:35:15 +0500

    Add files via upload

---
+++ SP20_BSE_065_Assi_002/app/src/main/java/com/example/sp20_bse_065_assi_002/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sp20_bse_065_assi_002;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void verify(View view) {
+    }
+}
